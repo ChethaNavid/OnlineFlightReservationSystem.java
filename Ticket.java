c092436f6a7fac41c2e34e35887b9daf397576e1
Samnang-Vouen <121634098+Samnang-Vouen@users.noreply.github.com>
2025-01-30 14:41:27 +0700

    Upload File

--- Ticket.java
+++ Ticket.java
@@ -1,32 +1,73 @@
 public class Ticket {
-    String passengerId;
-    String paymentId;
-    String airplaneCode;//"AB123"
-    String dateOfJourney; //YYYY-MM-DD
-    int seatNum; //can justify plane Class
-    String destination ;
-    String source;
-    String dateOfBuyingTicket;
-    String airlineName;
-    String ticketId;//
+    private String passengerId;
+    private String paymentId;
+    private String airplaneCode;//"AB123"
+    private String dateOfJourney; //YYYY-MM-DD
+    private int seatNum; //can justify plane Class
+    private String destination ;
+    private String source;
+    private String dateOfBuyingTicket;
+    private String airlineName;
+    private int ticketId;
 
-    static int totalTickets = 0;
+    private static int totalTickets = 0;
 
-    Ticket(String passId, String payment, String planeCode, String doj, int sn, String to, String from, String dobt, String airName){
-        passengerId = passId;
-        paymentId = payment;
-        airplaneCode = planeCode;
-        dateOfJourney = doj;
-        seatNum = sn;
-        destination = to;
-        source = from;
-        dateOfBuyingTicket = dobt;
-        airlineName = airName;
-        totalTickets++;
+    public Ticket(String passId, String payment, String planeCode, String doj, int sn, String to, String from, String dobt, String airName){
+        this.passengerId = passId;
+        this.paymentId = payment;
+        this.airplaneCode = planeCode;
+        this.dateOfJourney = doj;
+        this.seatNum = sn;
+        this.destination = to;
+        this.source = from;
+        this.dateOfBuyingTicket = dobt;
+        this.airlineName = airName;
+
+        this.ticketId = ++totalTickets;
+    }
+
+    public String getPassengerId() { 
+        return passengerId; 
+    }
+
+    public String getPaymentId() { 
+        return paymentId; 
+    }
+
+    public String getAirplaneCode() { 
+        return airplaneCode; 
+    }
+
+    public String getDateOfJourney() { 
+        return dateOfJourney; 
+    }
+
+    public int getSeatNum() { 
+        return seatNum; 
+    }
+
+    public String getDestination() { 
+        return destination; 
+    }
+
+    public String getSource() { 
+        return source; 
+    }
+
+    public String getDateOfBuyingTicket() { 
+        return dateOfBuyingTicket; 
+    }
+
+    public String getAirlineName() { 
+        return airlineName; 
+    }
+
+    public int getTicketId() { 
+        return ticketId; 
     }
 
     void newTicket(){
-        System.out.println("New ticket created successfully.");
+        System.out.println("New ticket created successfully. Ticket ID: " + ticketId);
     }
 
     void deleteTicket(){
