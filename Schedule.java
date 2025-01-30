92203c02129aaa7ec9c85ec58676fe4f70f37ec3
Zorina69 <soychanratana14@gmail.com>
2025-01-30 15:40:12 +0700

    roth

--- Schedule.java
+++ Schedule.java
@@ -9,6 +9,21 @@ public class Schedule {
     private String time;
     private String airlineName;
 
+    private void checkFlightDetails( String date) {
+        if (date.equals("10/01/2026")) {
+            System.out.println("Flight Details");
+            System.out.println("From: " + fromWhere);
+            System.out.println("To: " + toWhere);
+            System.out.println("Duration: " + durationTime);
+            System.out.println("Date: " + date);
+            System.out.println("Time: " + time);
+            System.out.println("Airline: " + airlineName);
+        } else {
+            System.out.println("No flight available on this date.");
+        }
+        
+    }
+
     private void setFlightDetails(String from, String to, String duration, String date, String Time, String airline) {
             this.fromWhere = from;
             this.toWhere = to;
@@ -20,5 +35,9 @@ public class Schedule {
 
     public static void main(String[] args) {
        Direction Direction1 = new Direction();
+    }
+
+    public Schedule(String airlineName) {
+        this.airlineName = airlineName;
     }
 }
