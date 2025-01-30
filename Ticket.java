1d9f384e5665bd52967d67924096ae3808b1ec9d
Samnang-Vouen <121634098+Samnang-Vouen@users.noreply.github.com>
2025-01-30 08:31:00 +0700

    Upload File

---
+++ Ticket.java
@@ -0,0 +1,39 @@
+public class Ticket {
+    String passengerId;
+    String paymentId;
+    String airplaneCode;//"AB123"
+    String dateOfJourney; //YYYY-MM-DD
+    int seatNum; //can justify plane Class
+    String destination ;
+    String source;
+    String dateOfBuyingTicket;
+    String airlineName;
+    String ticketId;//
+
+    static int totalTickets = 0;
+
+    Ticket(String passId, String payment, String planeCode, String doj, int sn, String to, String from, String dobt, String airName){
+        passengerId = passId;
+        paymentId = payment;
+        airplaneCode = planeCode;
+        dateOfJourney = doj;
+        seatNum = sn;
+        destination = to;
+        source = from;
+        dateOfBuyingTicket = dobt;
+        airlineName = airName;
+        totalTickets++;
+    }
+
+    void newTicket(){
+        System.out.println("New ticket created successfully.");
+    }
+
+    void deleteTicket(){
+        System.out.println("Ticket deleted successfully.");
+    }
+
+    static int getTotalTickets() {
+        return totalTickets;
+    }
+}
