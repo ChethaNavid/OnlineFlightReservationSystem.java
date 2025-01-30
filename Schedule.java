51d0349ea8b00738d65e713249bac4e6dd1f6ba4
Zorina69 <soychanratana14@gmail.com>
2025-01-30 15:14:40 +0700

    roth

---
+++ Schedule.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Schedule {
+    private Scanner input = new Scanner(System.in);
+    private String fromWhere;
+    private String toWhere;
+    private String durationTime;
+    private String date;
+    private String time;
+    private String airlineName;
+
+    private void setFlightDetails(String from, String to, String duration, String date, String Time, String airline) {
+            this.fromWhere = from;
+            this.toWhere = to;
+            this.durationTime = duration;
+            this.date = date;
+            this.time = Time;
+            this.airlineName = airline;
+    }
+
+    public static void main(String[] args) {
+       Direction Direction1 = new Direction();
+    }
+}
