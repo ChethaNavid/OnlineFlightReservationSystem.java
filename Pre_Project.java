1055d9ce2c9ca183ea7093d611218f3a8ac39da9
Zorina69 <soychanratana14@gmail.com>
2025-01-30 08:22:43 +0700

    Add files via upload

---
+++ Pre_Project.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Pre_Project {
+    public static void main(String[] args) {
+        System.out.println("\t\t\t\t\t" + "Welcome to my Project!");
+        System.out.println("\t\t\t\t" + "    " + "It's my first time to write java.");
+        System.out.println("\t\t\t\t\t" + "Press any key to continue...");
+    }
+    
+}
